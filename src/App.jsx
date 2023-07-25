7e0d8fc2c784ec76ce482e6c5a1255951f8624b9
Nyingambe Brian <briannmbe@gmail.com>
2023-07-25 23:11:26 +0300

    imlement the two cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action is Unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
-    fetch("http://localhost:3000/questions")
+    fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("Error"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "Error" }));
   }, []);
 
   return (
